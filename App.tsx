01fcfa36f231e3762d4eb74b532ab1b7b6cfc6d6
Instituto-Fibonacci <ifep@institutofibonacci.com.br>
2026-09-12 16:48:55 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
 import { Toaster } from "sonner";
-import { Route, Switch } from "wouter";
-import Home from "./pages/Home";
-
-function NotFound() {
-  return <div style={{ padding: 40, fontFamily: "sans-serif" }}>Página não encontrada.</div>;
-}
+import Home from "./Home";
+import ErrorBoundary from "./ErrorBoundary";
 
 export default function App() {
   return (
-    <>
-      <Toaster position="bottom-center" richColors />
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </>
+    <ErrorBoundary>
+      <Home />
+      <Toaster position="top-right" />
+    </ErrorBoundary>
   );
 }
