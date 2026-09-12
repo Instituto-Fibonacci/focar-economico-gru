20e9ed380a7d03275a2b8188be826b4527ea7d00
Instituto-Fibonacci <ifep@institutofibonacci.com.br>
2026-09-12 16:50:48 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import { Toaster } from "sonner";
 import Home from "./Home";
-import ErrorBoundary from "./ErrorBoundary";
 
 export default function App() {
   return (
-    <ErrorBoundary>
+    <>
       <Home />
       <Toaster position="top-right" />
-    </ErrorBoundary>
+    </>
   );
 }
